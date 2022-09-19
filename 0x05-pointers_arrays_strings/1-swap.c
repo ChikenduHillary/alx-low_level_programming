e4446728005e1c3e319a0a1e66e49b17e5d363e5
Chikendu Hillary <cikenduhill@mail.com>
2022-09-19 09:37:05 -0700

    c pointers strings and arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps integers
+ *
+ * @a: first integer
+ * @b: second interger
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *b;
+	int d = *a;
+	*a = c;
+	*b = d;
+}
