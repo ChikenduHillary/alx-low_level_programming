6c99b66109ae33de60d6f3cdf2453baaee292816
Chikendu Hillary <cikenduhill@mail.com>
2022-09-28 08:06:32 -0700

    C Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,49 +1,39 @@
 #include "main.h"
 /**
- * length - helps do it
- * @s: string
- * Return: string length
+ * _strlen_recursion - function that return the length of a string
+ * @s: the string
+ * Return: length of s
  */
-int length(char *s)
+int _strlen_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s)
-	{
-		i = i + length(s + 1);
-		return (i += 1);
-	}
-	return (0);
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
 /**
- * helper2 - helps more
- * @i: integer i
+ * palcheck - checker
+ * @x: iterration
+ * @y: length of s
  * @s: string
- * Return: int value
+ * Return: 1 or 0
  */
-int helper2(int i, char *s)
+int palcheck(int x, int y, char *s)
 {
-	if (*s)
-	{
-		if (*s != s[length(s) - 1])
-		{
-			return (0);
-		}
-		else
-		{
-			return (helper2(i + 1, s + 1));
-		}
-	}
-	return (1);
+	if (x >= y)
+		return (1);
+	if (*(s + x) != *(s + y - x))
+		return (0);
+	return (palcheck(x + 1, y, s));
 }
 /**
- * is_palindrome - is it paldindrome
- * @s: string to check
- * Return: boolean
+ * is_palindrome - check if a string is a palindrome
+ * @s: string
+ * Return: 1 if s is a palindrome otherwise 0
  */
 int is_palindrome(char *s)
 {
-	int i = 1;
+	int i = _strlen_recursion(s);
 
-	return (helper2(i, s));
+	return (palcheck(0, i - 1, s));
 }
