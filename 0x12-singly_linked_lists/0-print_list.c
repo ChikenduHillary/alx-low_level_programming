aeef6f69b937152b67fd4a2133b2345ebc6e1db0
Chikendu Hillary <cikenduhill@mail.com>
2022-10-20 06:24:28 -0700

    c singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list
+ * @h: pointer
+ *
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->str);
+		}
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
